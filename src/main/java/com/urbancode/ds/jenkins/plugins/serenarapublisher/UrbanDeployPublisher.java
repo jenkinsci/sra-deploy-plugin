f4924e8c3a60a6f21721a5a58a11fde57a42ae2d
tpayne <tpayne@serena.com>
2013-04-19 11:43:46 +0100

    Initial import of the SRA deploy plugin

---
+++ src/main/java/com/urbancode/ds/jenkins/plugins/serenarapublisher/UrbanDeployPublisher.java
@@ -0,0 +1,377 @@
+/* ===========================================================================
+ *  Copyright (c) 2007 Serena Software. All rights reserved.
+ *
+ *  Use of the Sample Code provided by Serena is governed by the following
+ *  terms and conditions. By using the Sample Code, you agree to be bound by
+ *  the terms contained herein. If you do not agree to the terms herein, do
+ *  not install, copy, or use the Sample Code.
+ *
+ *  1.  GRANT OF LICENSE.  Subject to the terms and conditions herein, you
+ *  shall have the nonexclusive, nontransferable right to use the Sample Code
+ *  for the sole purpose of developing applications for use solely with the
+ *  Serena software product(s) that you have licensed separately from Serena.
+ *  Such applications shall be for your internal use only.  You further agree
+ *  that you will not: (a) sell, market, or distribute any copies of the
+ *  Sample Code or any derivatives or components thereof; (b) use the Sample
+ *  Code or any derivatives thereof for any commercial purpose; or (c) assign
+ *  or transfer rights to the Sample Code or any derivatives thereof.
+ *
+ *  2.  DISCLAIMER OF WARRANTIES.  TO THE MAXIMUM EXTENT PERMITTED BY
+ *  APPLICABLE LAW, SERENA PROVIDES THE SAMPLE CODE AS IS AND WITH ALL
+ *  FAULTS, AND HEREBY DISCLAIMS ALL WARRANTIES AND CONDITIONS, EITHER
+ *  EXPRESSED, IMPLIED OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, ANY
+ *  IMPLIED WARRANTIES OR CONDITIONS OF MERCHANTABILITY, OF FITNESS FOR A
+ *  PARTICULAR PURPOSE, OF LACK OF VIRUSES, OF RESULTS, AND OF LACK OF
+ *  NEGLIGENCE OR LACK OF WORKMANLIKE EFFORT, CONDITION OF TITLE, QUIET
+ *  ENJOYMENT, OR NON-INFRINGEMENT.  THE ENTIRE RISK AS TO THE QUALITY OF
+ *  OR ARISING OUT OF USE OR PERFORMANCE OF THE SAMPLE CODE, IF ANY,
+ *  REMAINS WITH YOU.
+ *
+ *  3.  EXCLUSION OF DAMAGES.  TO THE MAXIMUM EXTENT PERMITTED BY APPLICABLE
+ *  LAW, YOU AGREE THAT IN CONSIDERATION FOR RECEIVING THE SAMPLE CODE AT NO
+ *  CHARGE TO YOU, SERENA SHALL NOT BE LIABLE FOR ANY DAMAGES WHATSOEVER,
+ *  INCLUDING BUT NOT LIMITED TO DIRECT, SPECIAL, INCIDENTAL, INDIRECT, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, DAMAGES FOR LOSS OF
+ *  PROFITS OR CONFIDENTIAL OR OTHER INFORMATION, FOR BUSINESS INTERRUPTION,
+ *  FOR PERSONAL INJURY, FOR LOSS OF PRIVACY, FOR NEGLIGENCE, AND FOR ANY
+ *  OTHER LOSS WHATSOEVER) ARISING OUT OF OR IN ANY WAY RELATED TO THE USE
+ *  OF OR INABILITY TO USE THE SAMPLE CODE, EVEN IN THE EVENT OF THE FAULT,
+ *  TORT (INCLUDING NEGLIGENCE), STRICT LIABILITY, OR BREACH OF CONTRACT,
+ *  EVEN IF SERENA HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.  THE
+ *  FOREGOING LIMITATIONS, EXCLUSIONS AND DISCLAIMERS SHALL APPLY TO THE
+ *  MAXIMUM EXTENT PERMITTED BY APPLICABLE LAW.  NOTWITHSTANDING THE ABOVE,
+ *  IN NO EVENT SHALL SERENA'S LIABILITY UNDER THIS AGREEMENT OR WITH RESPECT
+ *  TO YOUR USE OF THE SAMPLE CODE AND DERIVATIVES THEREOF EXCEED US$10.00.
+ *
+ *  4.  INDEMNIFICATION. You hereby agree to defend, indemnify and hold
+ *  harmless Serena from and against any and all liability, loss or claim
+ *  arising from this agreement or from (i) your license of, use of or
+ *  reliance upon the Sample Code or any related documentation or materials,
+ *  or (ii) your development, use or reliance upon any application or
+ *  derivative work created from the Sample Code.
+ *
+ *  5.  TERMINATION OF THE LICENSE.  This agreement and the underlying
+ *  license granted hereby shall terminate if and when your license to the
+ *  applicable Serena software product terminates or if you breach any terms
+ *  and conditions of this agreement.
+ *
+ *  6.  CONFIDENTIALITY.  The Sample Code and all information relating to the
+ *  Sample Code (collectively "Confidential Information") are the
+ *  confidential information of Serena.  You agree to maintain the
+ *  Confidential Information in strict confidence for Serena.  You agree not
+ *  to disclose or duplicate, nor allow to be disclosed or duplicated, any
+ *  Confidential Information, in whole or in part, except as permitted in
+ *  this Agreement.  You shall take all reasonable steps necessary to ensure
+ *  that the Confidential Information is not made available or disclosed by
+ *  you or by your employees to any other person, firm, or corporation.  You
+ *  agree that all authorized persons having access to the Confidential
+ *  Information shall observe and perform under this nondisclosure covenant.
+ *  You agree to immediately notify Serena of any unauthorized access to or
+ *  possession of the Confidential Information.
+ *
+ *  7.  AFFILIATES.  Serena as used herein shall refer to Serena Software,
+ *  Inc. and its affiliates.  An entity shall be considered to be an
+ *  affiliate of Serena if it is an entity that controls, is controlled by,
+ *  or is under common control with Serena.
+ *
+ *  8.  GENERAL.  Title and full ownership rights to the Sample Code,
+ *  including any derivative works shall remain with Serena.  If a court of
+ *  competent jurisdiction holds any provision of this agreement illegal or
+ *  otherwise unenforceable, that provision shall be severed and the
+ *  remainder of the agreement shall remain in full force and effect.
+ * ===========================================================================
+ */
+
+package com.urbancode.ds.jenkins.plugins.serenarapublisher;
+
+import hudson.Extension;
+import hudson.Launcher;
+import hudson.model.BuildListener;
+import hudson.model.Result;
+import hudson.model.AbstractBuild;
+import hudson.tasks.BuildStepMonitor;
+import hudson.tasks.Notifier;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.ws.rs.core.UriBuilder;
+
+import org.kohsuke.stapler.DataBoundConstructor;
+
+/**
+ * <p> This class implements the UrbanDeploy publisher process by using the {@link
+ * com.urbancode.ds.jenkins.plugins.serenarapublisher.UrbanDeploySite}. </p>
+ */
+public class UrbanDeployPublisher extends Notifier {
+
+    /**
+     * Hold an instance of the Descriptor implementation for the UrbanDeploy Publisher.
+     */
+    @Extension
+    public static final UrbanDeployPublisherDescriptor DESCRIPTOR = new UrbanDeployPublisherDescriptor();
+
+    private String siteName;
+    private String component;
+    private String baseDir;
+    private String directoryOffset;
+    private String fileIncludePatterns;
+    private String fileExcludePatterns;
+    private String versionName;
+    private Boolean skip = false;
+    private Boolean deploy = false;
+    private String deployApp;
+    private String deployEnv;
+    private String deployProc;
+    private Map<String, String> envMap = null;
+
+    /**
+     * Default constructor
+     */
+    @DataBoundConstructor
+    public UrbanDeployPublisher(String siteName, String component, String versionName, String directoryOffset, String baseDir,
+                                String fileIncludePatterns, String fileExcludePatterns, Boolean skip, Boolean deploy,
+                                String deployApp, String deployEnv, String deployProc) {
+        this.component = component;
+        this.versionName = versionName;
+        this.baseDir = baseDir;
+        this.directoryOffset = directoryOffset;
+        this.fileIncludePatterns = fileIncludePatterns;
+        this.fileExcludePatterns = fileExcludePatterns;
+        this.siteName = siteName;
+        this.skip = skip;
+        this.deploy = deploy;
+        this.deployApp = deployApp;
+        this.deployEnv = deployEnv;
+        this.deployProc = deployProc;
+    }
+
+    public String getSiteName() {
+        String name = siteName;
+        if (name == null) {
+            UrbanDeploySite[] sites = DESCRIPTOR.getSites();
+            if (sites.length > 0) {
+                name = sites[0].getProfileName();
+            }
+        }
+        return name;
+    }
+
+    public void setSiteName(String siteName) {
+        this.siteName = siteName;
+    }
+
+    public String getComponent() {
+        return component;
+    }
+
+    public void setComponent(String component) {
+        this.component = component;
+    }
+
+    public String getBaseDir() {
+        return baseDir;
+    }
+
+    public void setBaseDir(String baseDir) {
+        this.baseDir= baseDir;
+    }
+
+    public String getDirectoryOffset() {
+        return directoryOffset;
+    }
+
+    public void setDirectoryOffset(String directoryOffset) {
+        this.directoryOffset = directoryOffset;
+    }
+
+    public String getFileIncludePatterns() {
+        if (fileIncludePatterns == null || fileIncludePatterns.trim().length() == 0) {
+            fileIncludePatterns = "**/*";
+        }
+        return fileIncludePatterns;
+    }
+
+    public void setFileIncludePatterns(String fileIncludePatterns) {
+        this.fileIncludePatterns = fileIncludePatterns;
+    }
+
+    public String getFileExcludePatterns() {
+        return fileExcludePatterns;
+    }
+
+    public void setFileExcludePatterns(String fileExcludePatterns) {
+        this.fileExcludePatterns = fileExcludePatterns;
+    }
+
+    public String getVersionName() {
+        return versionName;
+    }
+
+    public void setVersionName(String versionName) {
+        this.versionName = versionName;
+    }
+
+    public void setSkip(boolean skip) {
+        this.skip = skip;
+    }
+
+    public boolean isSkip() {
+        return skip;
+    }
+
+    public void setDeploy(boolean deploy) {
+        this.deploy = deploy;
+    }
+
+    public boolean isDeploy() {
+        return deploy;
+    }
+
+    public void setDeployApp(String deployApp) {
+        this.deployApp = deployApp;
+    }
+
+    public String getDeployApp() {
+        return deployApp;
+    }
+
+    public void setDeployEnv(String deployEnv) {
+        this.deployEnv = deployEnv;
+    }
+
+    public String getDeployEnv() {
+        return deployEnv;
+    }
+
+    public void setDeployProc(String deployProc) {
+        this.deployProc = deployProc;
+    }
+
+    public String getDeployProc() {
+        return deployProc;
+    }
+
+    /**
+     * This method returns the configured UrbanDeploySite object which match the siteName of the UrbanDeployPublisher
+     * instance. (see Manage Hudson and System Configuration point UrbanDeploy)
+     *
+     * @return the matching UrbanDeploySite or null
+     */
+    public UrbanDeploySite getSite() {
+        UrbanDeploySite[] sites = DESCRIPTOR.getSites();
+        if (siteName == null && sites.length > 0) {
+            // default
+            return sites[0];
+        }
+        for (UrbanDeploySite site : sites) {
+            if (site.getDisplayName().equals(siteName)) {
+                return site;
+            }
+        }
+        return null;
+    }
+
+
+    public BuildStepMonitor getRequiredMonitorService() {
+        return BuildStepMonitor.BUILD;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param build
+     * @param launcher
+     * @param listener
+     * @return
+     * @throws InterruptedException
+     * @throws java.io.IOException  {@inheritDoc}
+     * @see hudson.tasks.BuildStep#perform(hudson.model.Build, hudson.Launcher, hudson.model.BuildListener)
+     */
+
+    @Override
+    public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener)
+            throws InterruptedException, IOException {
+        if (isSkip()) {
+            listener.getLogger().println("Skip artifacts upload to SerenaRA - step disabled.");
+        }
+        else if (build.getResult() == Result.FAILURE || build.getResult() == Result.ABORTED) {
+            listener.getLogger().println("Skip artifacts upload to SerenaRA - build failed or aborted.");
+        }
+        else {
+            envMap = build.getEnvironment(listener);
+            
+            String resolvedComponent = resolveVariables(getComponent());
+            String resolvedBaseDir = resolveVariables(getBaseDir());
+            String resolvedVersionName = resolveVariables(getVersionName());
+            String resolvedFileIncludePatterns = resolveVariables(fileIncludePatterns);
+            String resolvedFileExcludePatterns = resolveVariables(fileExcludePatterns);
+            String resolvedDirectoryOffset = resolveVariables(directoryOffset);
+
+            UrbanDeploySite udSite = getSite();
+            try {
+                PublishArtifactsCallable task = new PublishArtifactsCallable(resolvedBaseDir, resolvedDirectoryOffset,
+                        udSite, resolvedFileIncludePatterns, resolvedFileExcludePatterns, resolvedComponent,
+                        resolvedVersionName, listener);
+                launcher.getChannel().call(task);
+
+                if (isDeploy()) {
+                    if (getDeployApp() == null || getDeployApp().trim().length() == 0) {
+                        throw new Exception("Deploy Application is a required field if Deploy is selected!");
+                    }
+                    if (getDeployEnv() == null || getDeployEnv().trim().length() == 0) {
+                        throw new Exception("Deploy Environment is a required field if Deploy is selected!");
+                    }
+                    if (getDeployProc() == null || getDeployProc().trim().length() == 0) {
+                        throw new Exception("Deploy Process is a required field if Deploy is selected!");
+                    }
+
+                    listener.getLogger().println("Starting deployment of " + getDeployApp() + " in " + getDeployEnv());
+                    createProcessRequest(udSite, resolvedComponent, resolvedVersionName);
+                }
+            }
+            catch (Throwable th) {
+                th.printStackTrace(listener.error("Failed to upload files" + th));
+                build.setResult(Result.UNSTABLE);
+            }
+        }
+
+        return true;
+    }
+
+    private void createProcessRequest(UrbanDeploySite site, String componentName, String versionName)
+            throws Exception {
+        URI uri = UriBuilder.fromPath(site.getUrl()).path("cli").path("applicationProcessRequest")
+                .path("request").build();
+        String json =
+                "{\"application\":\"" + getDeployApp() +
+                "\",\"applicationProcess\":\"" + getDeployProc() +
+                "\",\"environment\":\"" + getDeployEnv() +
+                "\",\"versions\":[{\"version\":\"" + versionName +
+                "\",\"component\":\"" + componentName + "\"}]}";
+        site.executeJSONPut(uri,json);
+
+    }
+
+    private String resolveVariables(String input) {
+        String result = input;
+        if (input != null && input.trim().length() > 0) {
+            Pattern pattern = Pattern.compile("\\$\\{[^}]*}");
+            Matcher matcher = pattern.matcher(result);
+            while (matcher.find()) {
+                String key = result.substring(matcher.start() + 2, matcher.end() - 1);
+                if (envMap.containsKey(key)) {
+                    result = matcher.replaceFirst(Matcher.quoteReplacement(envMap.get(key)));
+                    matcher.reset(result);
+                }
+            }
+        }
+
+        return result;
+    }
+}
+
+
